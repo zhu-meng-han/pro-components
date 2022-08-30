bbdead9c16ace82a8c046219fed4d4ea638c557c
朱孟韩 <menghan.zhu@outlook.com>
2022-08-30 10:35:56 +0800

    build: update

--- scripts/release.js
+++ scripts/release.js
@@ -38,13 +38,13 @@ async function release() {
   // Check npm registry
   logStep('check npm registry');
   const userRegistry = execa.sync('npm', ['config', 'get', 'registry']).stdout;
-  if (userRegistry.includes('https://registry.yarnpkg.com/')) {
-    printErrorAndExit(`Release failed, please use ${chalk.blue('npm run release')}.`);
-  }
-  if (!userRegistry.includes('https://registry.npmjs.org/')) {
-    const registry = chalk.blue('https://registry.npmjs.org/');
-    printErrorAndExit(`Release failed, npm registry must be ${registry}.`);
-  }
+  // if (userRegistry.includes('https://registry.yarnpkg.com/')) {
+  //   printErrorAndExit(`Release failed, please use ${chalk.blue('npm run release')}.`);
+  // }
+  // if (!userRegistry.includes('https://registry.npmjs.org/')) {
+  //   const registry = chalk.blue('https://registry.npmjs.org/');
+  //   printErrorAndExit(`Release failed, npm registry must be ${registry}.`);
+  // }
 
   let updated = null;
 
